be2acc9f9bcfba6d83d9bc7554e0c25aff4a1dcc
Alexander <alexzy_coder@mail.ru>
2023-02-13 12:13:56 +0700

    start project, add templates

---
+++ cons_int_store/cons_int_store/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cons_int_store project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cons_int_store.settings')
+
+application = get_wsgi_application()
